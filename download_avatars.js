7e494cfa6122134e744628cd6d639c481d3203f0
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-16 02:29:10 +0000

    added code to parse JSON data, sort by avatar and username, then right to a specified folder

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var {token} = require('./secrets')
+var fs = require('fs');
+var {token} = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -7,18 +8,44 @@ function getRepoContributors(repoOwner, repoName, cb) {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
       'User-Agent': 'request',
-      "Authorization": token
-    }
+      "Authorization": token,
+    },
+    json: true  //will automatically do JSON parse on results
   }
   // console.log(options.url);
   request.get(options, function(err, res, body) {
-  cb(err, body);
-});
+    cb(err, body);
+  });
 
 }
   
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, contributors) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log(JSON.stringify(result, null, 2));
+  for (const contributor of contributors) {
+    // console.log(contributor.login);
+    // console.log(contributor.avatar_url);
+    downloadImageByURL(contributor.avatar_url, `./avatar/${contributor.login}.jpg`);
+  }
 });
+
+
+function downloadImageByURL(url, filePath) {
+  console.log('Starting downloadImageByURL with', url, filePath);
+  request.get(url)               
+         .on('error', function (err) {                                   // Note 2
+           throw err; 
+         })
+         .on('response', function (response) {                           // Note 3
+           console.log('Response Status Code: ', response.statusCode);
+         })
+         .pipe(fs.createWriteStream(filePath));  
+}
+
+// const testurl = "https://avatars2.githubusercontent.com/u/89246?v=4"
+// const testPath = "./avatar/test.jpg"
+
+// console.log('Starting application!');
+// downloadImageByURL(testurl, testPath);
+
